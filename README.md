5c1b737c66c18e0c7893275ab5081a9697e7b9c3
kaahila <dr.doomxp@gmail.com>
2024-05-06 18:28:39 +0200

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+Based on https://github.com/VISWESWARAN1998/Simple-Yet-Hackable-WhatsApp-api
---
+++ main.py
@@ -0,0 +1,9 @@
+from whatsapp import WhatsApp
+
+whatsapp = WhatsApp(
+    10,
+    loginQrScreenshot="./screenshot.png",
+    session="C:\\Users\\therbold\PycharmProjects\Simple-Yet-Hackable-WhatsApp-api\session",
+)
+print(whatsapp.send_message("Bot Log", "This is a Bot test"))
+whatsapp.quit()
