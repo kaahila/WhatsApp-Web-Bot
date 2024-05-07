4eaa76391ed63e01b97f1afd0dbccd57a5ace8a9
kaahila <dr.doomxp@gmail.com>
2024-05-07 16:26:04 +0200

    changed wait time

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from whatsapp import WhatsApp
 
 whatsapp = WhatsApp(
-    10,
-    # loginQrScreenshot="./screenshot.png",
+    100,
+    loginQrScreenshot="./screenshot.png",
     session="C:\\Users\\therbold\PycharmProjects\Simple-Yet-Hackable-WhatsApp-api\session",
 )
 print(whatsapp.send_message("Bot Log", "This is a Bot test!"))
